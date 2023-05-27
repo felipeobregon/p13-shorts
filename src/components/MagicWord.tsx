e6a29ed273d18ec5ada0dadf6421e436ed3e84ca
felipe <felipe.a.obregon@gmail.com>
2023-05-26 20:39:35 -0400

    init commit

---
+++ src/components/MagicWord.tsx
@@ -0,0 +1,14 @@
+
+/*
+
+This is a word that you can click on and it will show you the definition of the word.
+
+*/
+
+export default function MagicWord() {
+    return (
+        <div>
+            <h1>Magic Word</h1>
+        </div>
+    )
+}
